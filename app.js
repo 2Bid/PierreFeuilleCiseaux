81cb31b1224302e62e0aa5f1b9f851b1bed43d6e
2Bid <48602153+2Bid@users.noreply.github.com>
2020-12-28 14:47:48 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,129 @@
+const buttons = document.getElementsByClassName('btn');
+let resultat = document.getElementById('resultat');
+
+for (let i=0; i < buttons.length; i++){
+
+   buttons[i].addEventListener("click", function jeu(){
+       // setTimeout(jeu, 2000);
+   const joueur = buttons[i].innerHTML;
+   const ia = buttons[Math.floor(Math.random()* buttons.length)].innerHTML;
+   $('#adversaire').fadeTo('normal', 0);
+   $('#joueur').fadeTo('normal', 0);
+// REsultat s'affiche trop tot
+
+
+  if (joueur ==='PIERRE' && ia === 'PIERRE'){
+       setTimeout(
+          function(){
+               $('#joueur').attr('src','/images/left-rock.png')&&
+               $('#adversaire').attr('src','/images/right-rock.png')
+          }, 350)
+        document.getElementById('resultat').innerHTML = "Egalité !";
+        document.getElementById('ia-choice').innerHTML = "L'ennemi a choisi la pierre !";
+        $('#adversaire').fadeTo('normal', 1);
+        $('#joueur').fadeTo('normal', 1);
+   }
+   else if (joueur ==='PIERRE' && ia === 'FEUILLE'){
+        setTimeout(
+             function(){
+                  
+        $('#joueur').attr('src','/images/left-rock.png')&&
+        $('#adversaire').attr('src','/images/right-paper.png')
+             }, 350
+        )
+        document.getElementById('resultat').innerHTML = "Perdu !";
+        document.getElementById('ia-choice').innerHTML = "L'ennemi a choisi la feuille !";
+        
+        $('#adversaire').fadeTo('normal', 1);
+        $('#joueur').fadeTo('normal', 1);
+   }
+   else if (joueur ==='PIERRE' && ia === 'CISEAUX'){
+        setTimeout(
+             function(){
+               $('#joueur').attr('src','/images/left-rock.png')&&
+               $('#adversaire').attr('src','/images/right-scissors.png')
+             }, 350
+        )
+        document.getElementById('resultat').innerHTML = "Gagné !";
+        document.getElementById('ia-choice').innerHTML = "L'ennemi a choisi les ciseaux !";
+        $('#adversaire').fadeTo('normal', 1);
+        $('#joueur').fadeTo('normal', 1);
+   }
+   else if (joueur ==='FEUILLE' && ia === 'PIERRE'){
+        setTimeout(
+             function(){
+                  
+               $('#joueur').attr('src','/images/left-paper.png')&&
+               $('#adversaire').attr('src','/images/right-rock.png');
+             },350
+        )
+        document.getElementById('resultat').innerHTML = "Gagné !";
+        document.getElementById('ia-choice').innerHTML = "L'ennemi a choisi la pierre !";
+        $('#adversaire').fadeTo('normal', 1);
+        $('#joueur').fadeTo('normal', 1);
+   }
+   else if (joueur ==='FEUILLE' && ia === 'FEUILLE'){
+        setTimeout(
+             function(){
+                  
+        $('#joueur').attr('src','/images/left-paper.png')&&
+        $('#adversaire').attr('src','/images/right-paper.png');
+             }, 350
+        )
+        document.getElementById('resultat').innerHTML = "Egalité !";
+        document.getElementById('ia-choice').innerHTML = "L'ennemi a choisi la feuille !";
+        $('#adversaire').fadeTo('normal', 1);
+        $('#joueur').fadeTo('normal', 1);
+   }
+   else if (joueur ==='FEUILLE' && ia === 'CISEAUX'){
+        setTimeout(
+             function(){
+                  
+        $('#joueur').attr('src','/images/left-paper.png')&&
+        $('#adversaire').attr('src','/images/right-scissors.png');
+             },350
+        )
+        document.getElementById('resultat').innerHTML = "Perdu !";
+        document.getElementById('ia-choice').innerHTML = "L'ennemi a choisi les ciseaux !";
+        $('#adversaire').fadeTo('normal', 1);
+        $('#joueur').fadeTo('normal', 1);
+   }
+   else if (joueur ==='CISEAUX' && ia === 'PIERRE'){
+        setTimeout(
+             function(){
+                  
+        $('#joueur').attr('src','/images/left-scissors.png')&&
+        $('#adversaire').attr('src','/images/right-rock.png');
+             } ,350
+        )
+        document.getElementById('resultat').innerHTML = "Perdu !";
+        document.getElementById('ia-choice').innerHTML = "L'ennemi a choisi la pierre !";
+        $('#adversaire').fadeTo('normal', 1);
+        $('#joueur').fadeTo('normal', 1);
+   }
+   else if (joueur ==='CISEAUX' && ia === 'FEUILLE'){
+        setTimeout(() => {
+             
+        $('#joueur').attr('src','/images/left-scissors.png')&&
+        $('#adversaire').attr('src','/images/right-paper.png')
+        }, 350);
+        document.getElementById('resultat').innerHTML = "Gagné !";
+        document.getElementById('ia-choice').innerHTML = "L'ennemi a choisi la feuille !";
+        $( "#scorejoueur" ).text( "test" );
+        $('#adversaire').fadeTo('normal', 1);
+        $('#joueur').fadeTo('normal', 1);
+   }
+   else if (joueur ==='CISEAUX' && ia === 'CISEAUX'){
+        setTimeout(() => {
+             
+        $('#joueur').attr('src','/images/left-scissors.png')&&
+        $('#adversaire').attr('src','/images/right-scissors.png');
+        }, 350);
+        document.getElementById('resultat').innerHTML = "Egalité !";
+        document.getElementById('ia-choice').innerHTML = "L'ennemi a choisi les ciseaux !";
+        $('#adversaire').fadeTo('normal', 1);
+        $('#joueur').fadeTo('normal', 1);
+   }
+});
+}
+
